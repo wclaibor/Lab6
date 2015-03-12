4dacbbeefa4045d10843f970191f37548025f4b8
Wes <root@ip-172-31-20-1.us-west-2.compute.internal>
2015-03-12 18:46:53 +0000

    lab7

--- lab6.js
+++ lab6.js
@@ -1,9 +1,53 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
+var readline = require('readline');
 var ROOT_DIR = "html/";
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
+  if(urlObj.pathname.indexOf("comment") !=-1) {
+    console.log("comment route");
+    if(req.method === "POST") {
+      var jsonData = "";
+      req.on('data', function (chunk) {
+	jsonData += chunk;
+      });
+      req.on('end', function() {
+	var reqObj = JSON.parse(jsonData);
+	console.log(reqObj);
+	console.log("Name: "+reqObj.Name);
+	console.log("Comment: "+reqObj.Comment);
+	var MongoClient = require('mongodb').MongoClient;
+	MongoClient.connect("mongodb://localhost/weather", function(err,db) {
+	  if(err) throw err;
+	  db.collection('comments').insert(reqObj,function(err,records) {
+	    console.log("Record added as "+records[0]._id);
+	  });
+	})
+      });
+      res.writeHead(200);
+      res.end("");
+    }
+    else if (req.method === "GET") {
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather",function(err,db) {
+	if(err) throw err;
+	db.collection("comments",function(err,comments) {
+	  if(err) throw err;
+	  comments.find(function(err,items) {
+	    items.toArray(function(err,itemArr) {
+	      console.log("Document Array: ");
+	      console.log(itemArr);
+	      res.writeHead(200);
+	      res.end(JSON.stringify(itemArr));
+	    });
+  	  });
+	});
+      });
+//      res.writeHead(200);
+//      res.end(JSON.stringify(itemArr));
+    }
+  } else {
   console.log("URL path "+urlObj.pathname);
   console.log("URL search "+urlObj.search);
   console.log("URL query "+urlObj.query["q"]);
@@ -38,6 +82,7 @@ http.createServer(function (req, res) {
     res.writeHead(200);
     res.end(data);
     });
+  }
   }
 }).listen(80);
 
